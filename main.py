9e175d62a139efa0a3084a897b8060d8c781a5ff
LynaSim <lyna.cloud@gmail.com>
2026-02-13 14:59:59 +0000

    User prompts all done and variables assigned to user input.

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
 
 project_title = inquirer.text(message="What is your project's title?: ").execute()
 print("variable check: " + project_title)
+description = inquirer.text(message="Please add a description: ").execute()
+print("variable check: " + description)
+install_instructions = inquirer.text("Installation instructions: ").execute()
+usage_info = inquirer.text(message="How to use: ").execute()
+license = inquirer.select(message="Select a license type: ", choices=[
+    "Creative Commons license family",
+    "Educational Community License v2.0",
+    "GNU General Public License family",
+    "MIT",
+    "Open Software License 3.0"
+]).execute()
+print("variable check: " + license)
+author = inquirer.text(message="Author's name: ").execute()
+contact = inquirer.text(message="Contact information: ").execute()
+print("Thank you for completing this!")
+
+
+
+
+
