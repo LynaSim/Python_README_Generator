dec132564cb4b5093eeed2820f64f88f6338314b
LynaSim <lyna.cloud@gmail.com>
2026-02-19 13:26:25 +0000

    input_gathering.py now working as it should to import dictionary with all user input.

--- input_gathering.py
+++ input_gathering.py
@@ -1,17 +1,18 @@
+from InquirerPy import inquirer
 
 def get_all_input():
 
     all_input = {}
 
     # Questions
-    project_title = inquirer.text(
+    all_input['project_title']= inquirer.text(
         message="What is your project's title?: ",
         validate = lambda result: len(result.strip()) > 0,
         invalid_message = "Input cannot be empty.",
         filter=lambda result: result.strip()
     ).execute()
 
-    description = inquirer.text(
+    all_input['description'] = inquirer.text(
         message="Please add a description: ", 
         multiline = True,
         validate = lambda result: len(result.strip()) > 0,
@@ -19,7 +20,7 @@ def get_all_input():
         filter=lambda result: result.strip()
     ).execute()
 
-    install_instructions = inquirer.text(
+    all_input['install_instructions'] = inquirer.text(
         message = "Installation instructions: ",
         multiline = True,
         validate = lambda result: len(result.strip()) > 0,
@@ -27,7 +28,7 @@ def get_all_input():
         filter=lambda result: result.strip()
     ).execute()
 
-    usage_info = inquirer.text(
+    all_input['usage_info'] = inquirer.text(
         message="How to use: ",
         multiline = True,
         validate = lambda result: len(result.strip()) > 0,
@@ -35,7 +36,7 @@ def get_all_input():
         filter=lambda result: result.strip(),
     ).execute()
 
-    license = inquirer.select(
+    all_input['license'] = inquirer.select(
         message="Select a license type: ",
         choices=[
         "Creative Commons license family",
@@ -45,16 +46,19 @@ def get_all_input():
         "Open Software License 3.0"]
     ).execute()
 
-    author = inquirer.text(
+    all_input['author'] = inquirer.text(
         message="Author's name: ",
         validate = lambda result: len(result.strip()) > 0,
         invalid_message = "Input cannot be empty.",
         filter=lambda result: result.strip()
     ).execute()
 
-    contact = inquirer.text(
+    all_input['contact'] = inquirer.text(
         message="Contact information: ",
         validate = lambda result: len(result.strip()) > 0,
         invalid_message = "Input cannot be empty.",
         filter=lambda result: result.strip()
     ).execute()
+
+    return all_input
+
--- new_main.py
+++ new_main.py
@@ -5,39 +5,42 @@ from rich.progress import track #progress bar
 import time
 import logging
 from rich.logging import RichHandler
+from input_gathering import get_all_input
 
 #Initialise RICH console
 console = Console()
 
+# Gather all inputs from prompts and store them in dictionary
+all_input = get_all_input()
+print(f"This is the value of dictionary all_input: {all_input}")
 
+# # Function progress bar simulation
+# def creating_file_simulation():
+#     console.print("[bold cyan]Creating your README.md file...[/bold cyan]")
+#     for _ in track(range(10), description="Processing..."):
+#         time.sleep(0.2)
 
-# Function progress bar simulation
-def creating_file_simulation():
-    console.print("[bold cyan]Creating your README.md file...[/bold cyan]")
-    for _ in track(range(10), description="Processing..."):
-        time.sleep(0.2)
+# # Launches the progress bar before the README is created
+# creating_file_simulation()
 
-# Launches the progress bar before the README is created
-creating_file_simulation()
+# # Function for pretty RICH logging information
+# def setup_logging():
+#     logging.basicConfig(
+#         level ="INFO", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+#     )
+#     logger = logging.getLogger("rich")
+#     return logger
 
-# Function for pretty RICH logging information
-def setup_logging():
-    logging.basicConfig(
-        level ="INFO", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
-    )
-    logger = logging.getLogger("rich")
-    return logger
+# logger = setup_logging()
 
-logger = setup_logging()
-
-# Creates a separate file and appends value of a variable in it
-# Adds markdown formatting
-with open("README.md", "a") as file:
-    file.write(f"# {project_title}  \n")
-    file.write(f"## Description  \n\n{description}  \n\n")
-    file.write(f"## How to Install  \n\n{install_instructions}  \n\n")
-    file.write(f"## How to Use  \n\n{usage_info}  \n\n")
-    file.write(f"## License Type  \n\n{license}  \n\n")
-    file.write(f"## Author  \n\n{author}  \n\n")
-    file.write(f"## Contact  \n\n{contact}  \n\n")
-    logger.info("Your README.md file was successfully created!")
+# # Creates a separate file and appends value of a variable in it
+# # Adds markdown formatting
+# with open("README.md", "a") as file:
+#     file.write(f"# {project_title}  \n")
+#     file.write(f"## Description  \n\n{description}  \n\n")
+#     file.write(f"## How to Install  \n\n{install_instructions}  \n\n")
+#     file.write(f"## How to Use  \n\n{usage_info}  \n\n")
+#     file.write(f"## License Type  \n\n{license}  \n\n")
+#     file.write(f"## Author  \n\n{author}  \n\n")
+#     file.write(f"## Contact  \n\n{contact}  \n\n")
+#     logger.info("Your README.md file was successfully created!")
