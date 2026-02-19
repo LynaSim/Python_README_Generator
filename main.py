5dc4c0e4f4144f28bf3f465af4c8783777c18d19
LynaSim <lyna.cloud@gmail.com>
2026-02-19 00:43:49 +0000

    Added validation to prevent empty user input (empty lines and blank spaces) + input value trimming.

--- main.py
+++ main.py
@@ -10,19 +10,60 @@ from rich.logging import RichHandler
 console = Console()
 
 # Questions
-project_title = inquirer.text(message="What is your project's title?: ").execute()
-description = inquirer.text(message="Please add a description: ").execute()
-install_instructions = inquirer.text("Installation instructions: ").execute()
-usage_info = inquirer.text(message="How to use: ").execute()
-license = inquirer.select(message="Select a license type: ", choices=[
+project_title = inquirer.text(
+    message="What is your project's title?: ",
+    validate = lambda result: len(result.strip()) > 0,
+    invalid_message = "Input cannot be empty.",
+    filter=lambda result: result.strip()
+).execute()
+
+description = inquirer.text(
+    message="Please add a description: ", 
+    multiline = True,
+    validate = lambda result: len(result.strip()) > 0,
+    invalid_message = "Input cannot be empty.",
+    filter=lambda result: result.strip()
+).execute()
+
+install_instructions = inquirer.text(
+    message = "Installation instructions: ",
+    multiline = True,
+    validate = lambda result: len(result.strip()) > 0,
+    invalid_message = "Input cannot be empty.",
+    filter=lambda result: result.strip()
+).execute()
+
+usage_info = inquirer.text(
+    message="How to use: ",
+    multiline = True,
+    validate = lambda result: len(result.strip()) > 0,
+    invalid_message = "Input cannot be empty.",
+    filter=lambda result: result.strip(),
+).execute()
+
+license = inquirer.select(
+    message="Select a license type: ",
+    choices=[
     "Creative Commons license family",
     "Educational Community License v2.0",
     "GNU General Public License family",
     "MIT",
-    "Open Software License 3.0"
-]).execute()
-author = inquirer.text(message="Author's name: ").execute()
-contact = inquirer.text(message="Contact information: ").execute()
+    "Open Software License 3.0"]
+).execute()
+
+author = inquirer.text(
+    message="Author's name: ",
+    validate = lambda result: len(result.strip()) > 0,
+    invalid_message = "Input cannot be empty.",
+    filter=lambda result: result.strip()
+).execute()
+
+contact = inquirer.text(
+    message="Contact information: ",
+    validate = lambda result: len(result.strip()) > 0,
+    invalid_message = "Input cannot be empty.",
+    filter=lambda result: result.strip()
+).execute()
 
 # Function progress bar simulation
 def creating_file_simulation():
